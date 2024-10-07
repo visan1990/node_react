9fbb0bb418f4cc43694ca053e4b3bb99c39e1b9b
visan1990 <vicsanrmz@gmail.com>
2024-10-07 08:09:47 -0600

    Configuración de conexión BD

--- node_express/database/conexion.js
+++ node_express/database/conexion.js
@@ -2,6 +2,7 @@ const mysql = require( 'mysql2' );
 
 const db = mysql.createConnection({
     host    : process.env.MYSQL_SERVER,
+    port    : process.env.MYSQL_PORT,
     user    : process.env.MYSQL_USER,
     password: process.env.MYSQL_PASS,
     database: process.env.MYSQL_DATABASE
